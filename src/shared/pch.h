2555fac1a61318f66840d4834aeec3273fa11999
Mikaël Capelle <capelle.mikael@gmail.com>
2024-06-15 10:59:47 +0200

    Update shared for C++20 and std::format.

--- src/shared/pch.h
+++ src/shared/pch.h
@@ -10,6 +10,7 @@
 #include <cstring>
 #include <ctime>
 #include <exception>
+#include <format>
 #include <functional>
 #include <future>
 #include <iomanip>
@@ -29,7 +30,7 @@
 #include <vector>
 
 #ifndef NOMINMAX
-  #define NOMINMAX
+#define NOMINMAX
 #endif
 
 #define WIN32_LEAN_AND_MEAN
@@ -77,9 +78,6 @@
 #include <boost/thread/shared_lock_guard.hpp>
 #include <boost/tokenizer.hpp>
 #include <boost/type_traits.hpp>
-
-#include <fmt/format.h>
-#include <fmt/ostream.h>
 
 #include <spdlog.h>
 
