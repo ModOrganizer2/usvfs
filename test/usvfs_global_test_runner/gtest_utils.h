3063319ff06824ee28ceb6ffc613ecf6e4562ba3
Mikaël Capelle <capelle.mikael@gmail.com>
2024-06-22 19:33:55 +0200

    Add comments.

--- test/usvfs_global_test_runner/gtest_utils.h
+++ test/usvfs_global_test_runner/gtest_utils.h
@@ -15,8 +15,15 @@
                                                const std::filesystem::path& path,
                                                bool trim = true);
 
+// macro to assert that the contents of two directories are identical - directories are
+// compared recursively and file contents are compared (excluding extra spaces or lines
+// in file)
+//
 #define ASSERT_DIRECTORY_EQ(Expected, Actual)                                          \
   ASSERT_TRUE(AssertDirectoryEquals(Expected, Actual))
 
+// macro to assert that the contents of two files are identical (excluding extra space
+// or lines in file)
+//
 #define ASSERT_CONTENT_EQ(Expected, Path)                                              \
   ASSERT_TRUE(AssertContentEquals(Expected, Path))
