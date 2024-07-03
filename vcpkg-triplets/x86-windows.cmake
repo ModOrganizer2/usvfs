2942d9c38d106ab0795398a0d1a59eb7d285fb00
Mikaël Capelle <capelle.mikael@gmail.com>
2024-07-03 16:42:46 +0200

    Tentative work for asmjit, currently fails.

---
+++ vcpkg-triplets/x86-windows.cmake
@@ -0,0 +1,11 @@
+set(VCPKG_TARGET_ARCHITECTURE x86)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+
+if(PORT MATCHES "^boost-")
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+
+if(PORT MATCHES "asmjit")
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
